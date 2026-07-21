d64a078e56e60ebca6f36dc1826ef0a091658167
RoRoWeRwEr <rorowerwer@gmail.com>
2026-07-21 15:22:33 +0300

    Update 0003_create_timestamp_function.sql

--- supabase/migrations/0003_create_timestamp_function.sql
+++ supabase/migrations/0003_create_timestamp_function.sql
@@ -1,1 +1,9 @@
-timezone('utc', now())
+CREATE OR REPLACE FUNCTION public.set_updated_at()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    NEW.updated_at = timezone('utc', now());
+    RETURN NEW;
+END;
+$$;
